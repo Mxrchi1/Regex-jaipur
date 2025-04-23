ef9df5e89a52b2f97e5d2fcb25e9fb543d05a6c9
Mxrchi1 <abhibajrangsaraswatradheradhe@gmail.com>
2025-04-23 23:26:37 +0530

    the Q1 was solved

---
+++ assigntmen23-4-25.py
@@ -0,0 +1,6 @@
+# Q1 Write a program that takes an integer input from the user and checks whether the number is odd or even.
+a= int(input("enter a number "))
+if(a%2==0):
+    print("the number is even")
+else:
+    print("the number is odd")
