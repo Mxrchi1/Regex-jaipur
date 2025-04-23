4971d2e460a46bfd6d5b0b3b6ed073e227a2babd
Mxrchi1 <abhibajrangsaraswatradheradhe@gmail.com>
2025-04-23 23:35:32 +0530

    Q2 was solved

--- assigntmen23-4-25.py
+++ assigntmen23-4-25.py
@@ -4,3 +4,14 @@ if(a%2==0):
     print("the number is even")
 else:
     print("the number is odd")
+
+# Q2 Write a program that takes three numbers as input and prints the largest of the three.
+a = int(input("enter your values 1 "))
+b = int(input("enter your values 2 "))
+c = int(input("enter your values 3 "))
+if (a>b and a>c ):
+    print("a is greater")
+elif (b>a and b>c ):
+    print("b is greater")
+elif (c>a and c>b ):
+    print("c is greater")
